cd7fbcf173026064bd75cf391d400e832d16703b
koduki <pascalm3+git@gmail.com>
2023-11-10 08:28:37 +0900

    Add base html

--- index.ts
+++ index.ts
@@ -3,7 +3,12 @@ import { serveStatic } from 'https://deno.land/x/hono@v3.3.0/middleware.ts'
 
 const app = new Hono();
 
-app.use('/', serveStatic({ root: './public' }));
+app.use('/*', serveStatic({ root: './public' }));
+// app.use('/static/*', serveStatic({ root: './public/static' }));
+
+app.get('/signup', serveStatic({ path: './public/signup.html' }));
+app.get('/profile', serveStatic({ path: './public/profile.html' }));
+
 app.get('/echo', (c) => {
     return c.text('Hello Deno!')
 })
