1c14dbbbc18a4ee6f60d3b6b87ae5d5efdc96938
koduki <pascalm3+git@gmail.com>
2023-11-11 12:39:34 +0900

    Add device auth

--- index.ts
+++ index.ts
@@ -68,6 +68,28 @@ app.get('/generate-registration-options', (c) => {
     return c.json(options)
 });
 
+/**
+ * Login (a.k.a. "Authentication")
+ */
+app.get('/generate-authentication-options', (c) => {
+
+    const options = {
+        challenge: isoBase64URL.fromString(genRandomStr(32)),
+        allowCredentials: user.devices.map((dev) => ({
+            id: dev.credentialID,
+            type: 'public-key',
+            transports: dev.transports,
+        })),
+        timeout: 60000,
+        userVerification: 'required',
+        extensions: undefined,
+        rpId: 'localhost'
+    }
+
+    dummyStore.currentChallenge = options.challenge;
+    return c.json(options)
+});
+
 app.post('/verify-registration', async (c) => {
     const body: RegistrationResponseJSON = await c.req.json();
     const expectedChallenge = dummyStore.currentChallenge;
@@ -90,28 +112,80 @@ app.post('/verify-registration', async (c) => {
 
     const { verified, registrationInfo } = verification;
 
-    // if (verified && registrationInfo) {
-    //     const { credentialPublicKey, credentialID, counter } = registrationInfo;
-    //     const existingDevice = user.devices.find((device) =>
-    //         isoUint8Array.areEqual(device.credentialID, credentialID)
-    //     );
+    if (verified && registrationInfo) {
+        const { credentialPublicKey, credentialID, counter } = registrationInfo;
+        console.log(credentialID);
+        const existingDevice = user.devices.find((device) =>
+            isoUint8Array.areEqual(device.credentialID, credentialID)
+        );
 
-    //     if (!existingDevice) {
-    //         /**
-    //          * Add the returned device to the user's list of devices
-    //          */
-    //         const newDevice: AuthenticatorDevice = {
-    //             credentialPublicKey,
-    //             credentialID,
-    //             counter,
-    //             transports: body.response.transports,
-    //         };
-    //         user.devices.push(newDevice);
-    //     }
-    // }
+        if (!existingDevice) {
+            /**
+             * Add the returned device to the user's list of devices
+             */
+            const newDevice: AuthenticatorDevice = {
+                credentialPublicKey,
+                credentialID,
+                counter,
+                transports: body.response.transports,
+            };
+            user.devices.push(newDevice);
+        }
+    }
 
     dummyStore.currentChallenge = '';
     return c.json({ verified })
 });
+
+app.post('/verify-authentication', async (c) => {
+
+    const body: AuthenticationResponseJSON = await c.req.json();
+
+    const expectedChallenge = dummyStore.currentChallenge;
+
+    let dbAuthenticator;
+    const bodyCredIDBuffer = isoBase64URL.toBuffer(body.rawId);
+    // "Query the DB" here for an authenticator matching `credentialID`
+    for (const dev of user.devices) {
+        if (isoUint8Array.areEqual(dev.credentialID, bodyCredIDBuffer)) {
+            dbAuthenticator = dev;
+            break;
+        }
+    }
+
+    if (!dbAuthenticator) {
+        return new Response('Authenticator is not registered with this site', { status: 400 });
+    }
+
+    let verification: VerifiedAuthenticationResponse;
+    try {
+        const opts: VerifyAuthenticationResponseOpts = {
+            response: body,
+            expectedChallenge: `${expectedChallenge}`,
+            expectedOrigin,
+            expectedRPID: rpID,
+            authenticator: dbAuthenticator,
+            requireUserVerification: true,
+        };
+        verification = await verifyAuthenticationResponse(opts);
+    } catch (error) {
+        const _error = error as Error;
+        console.error(_error);
+        return new Response(_error.message, { status: 400 });
+
+    }
+
+    const { verified, authenticationInfo } = verification;
+
+    if (verified) {
+        // Update the authenticator's counter in the DB to the newest count in the authentication
+        dbAuthenticator.counter = authenticationInfo.newCounter;
+    }
+
+    dummyStore.currentChallenge = '';
+    console.log(verification)
+    return c.json({ verified })
+});
+
 
 Deno.serve(app.fetch)
---
+++ public/static/base64url.js
@@ -0,0 +1,25 @@
+export function ab2base64url(ab) {
+    const str = String.fromCharCode.apply(null, new Uint8Array(ab));
+    const base64 = window.btoa(str);
+    const base64url = base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=*$/g, '');
+
+    return base64url;
+}
+
+export function base64url2ab(base64url) {
+    // base64url to base 64
+    let base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+    const paddingLength = 4 - (base64.length % 4);
+    if (paddingLength !== 4) {
+        base64 += '='.repeat(paddingLength);
+    }
+
+    // base64 to ArrayBuffer
+    const bstr = atob(base64);
+    const ab = Uint8Array.from(bstr, str => str.charCodeAt(0)); // Uint8Array化
+    return ab;
+}
+
+export function s2ab(str) {
+    return (new TextEncoder('utf-8')).encode(str);
+}
